6b84afa262795b49d241b3dfd11c1bed5e24d0c1
luhui <luhui_seu@163.com>
2014-03-17 16:33:18 +0800

    commit romanToInteger

--- romanToInteger/romanToInteger.h
+++ romanToInteger/romanToInteger.h
@@ -7,83 +7,33 @@ using namespace std;
 /*** this is an inefficient method ***/
 class Solution {
 public:
-    int selectPos(int pos1,int pos2){
-        if(pos1 < 0 || pos2 < 0)
-            return  max(pos1,pos2);
-        else
-            return min(pos1,pos2);
-    }
     int romanToInt(string s) {
-        if(s.empty())
-            return 0;
-        unsigned int len = s.size();
-        unordered_map<string, int> m;
-        //string strArray[9];
-        if(s[0] == 'I' || s[0] == 'V'){ //The number is one digit
-            string strArray[9] = {"I","II" ,"III", "IV","V",
-                "VI","VII","VIII", "IX"};
-            for(int i = 0;i < 9; ++i)
-                m[strArray[i]] = i;
-           return m[s] + 1;
+        int result = 0;
+        unordered_map <char,int> mapToNum1;
+        mapToNum1.insert(make_pair('I', 1));
+        mapToNum1.insert(make_pair('V', 5));
+        mapToNum1.insert(make_pair('X', 10));
+        mapToNum1.insert(make_pair('L', 50));
+        mapToNum1.insert(make_pair('C', 100));
+        mapToNum1.insert(make_pair('D', 500));
+        mapToNum1.insert(make_pair('M', 1000));
+        unordered_map<char,int> mapToNum2;
+        mapToNum2.insert(make_pair('I',-1));
+        mapToNum2.insert(make_pair('X',-10));
+        mapToNum2.insert(make_pair('C',-100));
+        int len = s.size();
+        bool flag = false;
+        for(int i = len-1; i >= 0; --i){
+            flag = (s[i] == 'I' && result >= 5) || (s[i] == 'X' && result >= 50)
+                    || (s[i] == 'C' && result >= 500);
+            if(flag)
+                result += mapToNum2[s[i]];
+            else
+                result += mapToNum1[s[i]];
         }
-        else if(s[0] == 'X' || s[0] == 'L'){ // The int is two digits
-            string strArray[9] = {"X","XX" ,"XXX", "XL","L",
-                "LX","LXX","LXXX", "XC"};
-            int pos1 = s.find('I');
-            int pos2 = s.find('V');
-            if( pos1== s.npos && pos2 == s.npos){
-                for(int i = 0;i < 9; ++i)
-                    m[strArray[i]] = i;
-                return 10*(m[s] + 1);
-            }
-            else {
-                int pos = selectPos(pos1,pos2);
-                string tens = s.substr(0,pos);
-                string ones = s.substr(pos,len - pos);
-                return romanToInt(tens) + romanToInt(ones);
-            }
 
-        }
-        else if(s[0] == 'C' || s[0] == 'D'){
-            string strArray[9] = {"C","CC" ,"CCC", "CD","D",
-                "DC","DCC","DCCC", "CM"};
-            int pos1 = s.find('X');
-            int pos2 = s.find('L');
-            int pos3 = s.find('I');
-            if(pos3 != -1 && pos3 < pos1)
-                pos1 = pos2 = -1;
-            int pos;
-            if(pos1 == -1 && pos2 == -1){ //there is no tens digit
-                pos1 = s.find('I');
-                pos2 = s.find('V');
-                if(pos1 == -1 && pos2 == -1)
-                {
-                    for(int i = 0;i < 9; ++i)
-                    m[strArray[i]] = i;
-                    return 100*(m[s] + 1);
-                }
-                pos =selectPos(pos1,pos2);
-                string hundreds = s.substr(0,pos);
-                string tens = s.substr(pos,len - pos);
-                return romanToInt(hundreds) + romanToInt(tens);
-            }
-            else{
-                pos =selectPos(pos1,pos2);
-                string hundreds = s.substr(0,pos);
-                string tens = s.substr(pos,len - pos);
-                return romanToInt(hundreds) + romanToInt(tens);
-            }
-        }
-        else{
-            unsigned int mCount = 0;
-            for(; mCount < len; ++mCount)
-                if(s[mCount] != 'M')
-                    break;
-            return 1000 * mCount + romanToInt(s.substr(mCount,len - mCount));
-
-        }
+        return result;
     }
-
 };
 
 #endif // ROMANTOINTEGER_H_INCLUDED
